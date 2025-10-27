b2386976c45c7d91b7c8066cb1d3cc76aa416830
MUKILTEO\1717487 <fgreenmun@gmail.com>
2025-10-27 14:30:41 -0700

    a

--- Assets/WaveSystem2D.cs
+++ Assets/WaveSystem2D.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class WaveSystem2D : MonoBehaviour
 {
+    public float waveNumber = 1;
+    public float enemiesToSpawn = 5;
+    public float EnemiesAlive = 0;
+    public bool waveInProgress = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
